06e9d47aaac6e942edfe4b7c3daec0218982107a
DonAntonio04 <berrellezantonio04@gmail.com>
2023-12-11 02:06:51 -0700

    primero

---
+++ Arreglos_Producto/Arreglos_Producto/Arreglo_Producto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos_Producto
+{
+    public class Arreglo_Producto
+    {
+        Producto[] productos = new Producto[4];
+
+        public void Insertar(Producto valor, int posicion)
+        {
+            if(posicion < -1 || posicion > productos.Length)
+            {
+                Console.WriteLine("No puedes ingresar ese dato");
+            }
+            Console.WriteLine($"Se ingreso el nombre: {valor.Nombre} y el precio: {valor.Precio}");
+            productos[posicion] = valor;
+        }
+        public void OrdenarCreciente()
+        {
+            for(int i = 0; i < productos.Length - 1; i++)
+            {
+                for(int j = 0; j < productos.Length - i - 1; j++)
+                {
+                    if (productos[j]?.Precio > productos[j + 1]?.Precio)
+                    {
+                        Producto temp = productos[j];
+                        productos[j] = productos[j + 1];
+                        productos[j + 1] = temp;
+                    }
+                   
+                }
+            }
+        }
+        public void Mostrar()
+        {
+            for(int i = 0; i < productos.Length; i++)
+            {
+                Console.WriteLine($"Posición {i}: {productos[i]}");
+            }
+        }
+        public void BuscarPrecio(int precio)
+        {
+            bool encontrado = false;
+            for(int i = 0; i < productos.Length;i++)
+            {
+                if (productos[i]?.Precio == precio)
+                {
+                    Console.WriteLine($"El precio se se encuentra en la posicion: {i}");
+                    encontrado = true;
+                    break;
+                }
+            }
+            if(!encontrado)
+            {
+                Console.WriteLine("No se encuentra ese precio");
+            }
+        }
+    }
+}
---
+++ Arreglos_Producto/Arreglos_Producto/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Arreglos_Producto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arreglo_Producto arregloProducto = new Arreglo_Producto();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Ingrese los datos para el producto {i + 1}");
+
+                Console.Write("Nombre del producto: ");
+                string nombre = Console.ReadLine();
+
+       
+                Console.Write("Precio del producto: ");
+                int precio = Convert.ToInt32(Console.ReadLine());
+
+       
+                Producto nuevoProducto = new Producto(nombre, precio);
+
+                Console.Write("Posición en el arreglo: ");
+                int posicion = Convert.ToInt32(Console.ReadLine());
+
+            
+                arregloProducto.Insertar(nuevoProducto, posicion);
+            }
+
+
+            Console.WriteLine("Arreglo antes de ordenar:");
+            arregloProducto.Mostrar();
+
+   
+            arregloProducto.OrdenarCreciente();
+
+
+            Console.WriteLine("Arreglo después de ordenar:");
+            arregloProducto.Mostrar();
+
+          
+            Console.Write("Ingrese un precio para buscar: ");
+            int precioBuscar = Convert.ToInt32(Console.ReadLine());
+
+            arregloProducto.BuscarPrecio(precioBuscar);
+
+        
+        }
+    }
+}
